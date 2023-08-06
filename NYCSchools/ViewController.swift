a57162e540d66a3b5f2106d1a407c4fef459e368
Emerson Siega <emerson.siega@genial.com.vc>
2023-08-06 09:44:24 -0300

    Initial Commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by emersonsiega on 06/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
