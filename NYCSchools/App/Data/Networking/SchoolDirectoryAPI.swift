1ad21d3e9711a21062204010af46ff0ec2dccb77
Emerson Siega <emersonsiega@hotmail.com>
2023-08-08 08:49:13 -0300

    feat: Call API using Apple standard service

--- NYCSchools/App/Data/Networking/SchoolDirectoryAPI.swift
+++ NYCSchools/App/Data/Networking/SchoolDirectoryAPI.swift
@@ -18,6 +18,45 @@ class SchoolAPI: SchoolAPILogic {
     }
         
     func getSchools() {
-        print("Implement getSchools! \(Constants.schoolListUrl)")
+        retrieveSchoolsUsingStandardServices()
+    }
+    
+    private func retrieveSchoolsUsingStandardServices() {
+        /*
+        // Create url using URLComponents
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "data.cityofnewyork.us"
+        urlComponents.path = "resource/s3k6-pzi2.json"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "$$app_token", value: "L1KwLSwm1yz1N7aWqFCF4dLmM")
+        ]
+
+        if let url = urlComponents.url {
+          // Do something
+        }
+        */
+        
+        if let url = URL(string: Constants.schoolListUrl) {
+            let urlSession = URLSession(configuration: .default)
+            let task = urlSession.dataTask(with: url) { data, response, error in
+                guard error == nil else {
+                    print("Error occured \(error!.localizedDescription)")
+                    return
+                }
+                
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let schools = try decoder.decode([School].self, from: data)
+                        print("schools: \(schools)")
+                    } catch let error {
+                        print("Error during parsing \(error)")
+                    }
+                }
+            }
+            
+            task.resume()
+        }
     }
 }
